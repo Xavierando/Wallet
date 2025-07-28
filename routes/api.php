fb7e59e0432b3b0479b1f662940d98eb9528516b
Xavier Prot <prot.xavier@gmail.com>
2025-07-28 12:03:30 +0200

    add route name for authentications

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\Api\V1\TransactionController;
 use App\Http\Controllers\Api\V1\WalletController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/token', [AuthClientController::class, 'getToken']);
-Route::delete('/token', [AuthClientController::class, 'deleteToken'])->middleware('auth:sanctum');
+Route::post('/token', [AuthClientController::class, 'getToken'])->name('api.login');
+Route::delete('/token', [AuthClientController::class, 'deleteToken'])->middleware('auth:sanctum')->name('api.logout');
 
-Route::post('/emploie/token', [AuthEmploieController::class, 'getToken']);
-Route::delete('/emploie/token', [AuthEmploieController::class, 'deleteToken'])->middleware('auth:sanctum');
+Route::post('/emploie/token', [AuthEmploieController::class, 'getToken'])->name('api.emploie.login');
+Route::delete('/emploie/token', [AuthEmploieController::class, 'deleteToken'])->middleware('auth:sanctum')->name('api.emploie.logout');
 
 Route::name('apiv1.')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('wallets', WalletController::class);

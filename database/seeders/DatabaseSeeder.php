1496ca0e89d66101050e47e71872de95c935f6ea
Xavier Prot <prot.xavier@gmail.com>
2025-07-28 12:03:08 +0200

    mod seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,6 +32,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Wallet::factory()->count(100)->create();
-        Transaction::factory()->count(100)->create();
+        Transaction::factory()->count(500)->create();
     }
 }
